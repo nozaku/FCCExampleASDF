c56d389174ebbbecc28179677f658f1ecd8839c8
Matt Osteen <themosteen@gmail.com>
2016-05-22 16:07:05 +0900

    Start

---
+++ greeting.js
@@ -0,0 +1,23 @@
+function reverse(str) {
+  answer = str.split("").reverse().join("")
+  return answer
+}
+
+reverse("butts")
+
+//I made it. Yeah
+
+var butts = "My Butt!"
+ 
+ function whatAmISittingOn () {
+ 	return butts;
+ }
+ 
+ 	whatAmISittingOn();
+ 	
+ 	function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  result = "The " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + " towards the butt.";
+ 
+   return result;
+ }
